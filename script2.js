82cdb1f1c8de99256be568c1c988ba313afc5fa7
alialnaser <alialnaser1233@gmail.com>
2022-08-23 00:49:13 +0300

    did some of the hw3

---
+++ script2.js
@@ -0,0 +1,26 @@
+console.log("hello");
+
+function log1() {
+    console.log(7);
+  };
+
+  function log2() {
+    console.log(5);
+  };
+
+  function log3() {
+    console.log(6);
+  };
+
+  function log4() {
+    console.log(5);
+  };
+
+  function log5() {
+    console.log(6);
+  };
+
+  function log6() {
+    console.log(6);
+  };
+
